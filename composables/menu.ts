60ac6b167c70f30f15c906305dc016b74306d389
lSagenav <jcvanegas@unac.edu.co>
2025-05-07 21:00:12 -0500

    se resuelve el conflicto

--- composables/menu.ts
+++ composables/menu.ts
@@ -1,20 +1,26 @@
 export const menuItems = [
-    {
-      title: 'Dashboard',
-      icon: 'mdi-view-dashboard',
-      to: '/dashboard',
-    },
-    {
-      title: 'Admin',
-      icon: 'mdi-account-group',
-      children: [
-        {title: 'Usuarios', to: '/Admin' }
-      ],
-    },
-    {
-      title: 'Configuraciones',
-      icon: 'mdi-cog',
-      to: '/settings',
-    },
-  ]
-  
+  {
+    title: 'Dashboard',
+    icon: 'mdi-view-dashboard',
+    to: '/dashboard',
+  },
+  {
+    title: 'Empleos',
+    icon: 'mdi-briefcase',
+    children: [
+      { title: 'Empleos', to: '/empleos/Empleos' },
+    ],
+  },
+  {
+    title: 'Admin',
+    icon: 'mdi-account-group',
+    children: [
+      { title: 'Usuarios', to: '/Admin' }
+    ],
+  },
+  {
+    title: 'Configuraciones',
+    icon: 'mdi-cog',
+    to: '/settings',
+  },
+]
